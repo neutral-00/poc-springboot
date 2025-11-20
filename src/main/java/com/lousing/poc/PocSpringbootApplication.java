43a88add5c5a1175df515da6b17d8a8acc884bab
neutral00 <to.arvmail@gmail.com>
2025-11-20 15:36:28 +0530

    springboot poc project created

---
+++ src/main/java/com/lousing/poc/PocSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.lousing.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lousing/poc/PocSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lousing.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
