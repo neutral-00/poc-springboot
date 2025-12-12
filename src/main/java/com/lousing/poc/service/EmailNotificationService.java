228aae8d23513bd22ab427e6c5eb921548a63228
neutral00 <to.arvmail@gmail.com>
2025-12-12 19:50:02 +0530

    cleaning up older files

--- src/main/java/com/lousing/poc/service/EmailNotificationService.java
+++
@@ -1,8 +0,0 @@
-package com.lousing.poc.service;
-
-public class EmailNotificationService implements NotificationService{
-    @Override
-    public void send(String message, String recipient) {
-        System.out.println("📧 Email to " + recipient + ": " + message);
-    }
-}
--- src/main/java/com/lousing/poc/service/SmsNotificationService.java
+++
@@ -1,8 +0,0 @@
-package com.lousing.poc.service;
-
-public class SmsNotificationService implements NotificationService{
-    @Override
-    public void send(String message, String recipient) {
-        System.out.println("📱 SMS to " + recipient + ": " + message);
-    }
-}
