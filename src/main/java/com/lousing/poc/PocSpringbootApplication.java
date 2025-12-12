306c86ef63577b1d3c01fc638101d93d2e3c7ac1
neutral00 <to.arvmail@gmail.com>
2025-12-12 17:11:32 +0530

    main class updated to import the master config

--- src/main/java/com/lousing/poc/PocSpringbootApplication.java
+++ src/main/java/com/lousing/poc/PocSpringbootApplication.java
@@ -1,17 +1,20 @@
 package com.lousing.poc;
 
-import com.lousing.poc.config.NotificationConfig;
+import com.lousing.poc.config.NotificationMasterConfig;
+import com.lousing.poc.service.NotificationService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
-@Import(NotificationConfig.class)  // Load our Java config
+@Import(NotificationMasterConfig.class)  // ✅ Single import loads ALL configs
 public class PocSpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(PocSpringbootApplication.class, args);
-        System.out.println("✅ Beans configured via Java Config!");
+        System.out.println("✅ Multiple Config Files Loaded!");
         System.out.println("----------------------------------");
     }
 }
