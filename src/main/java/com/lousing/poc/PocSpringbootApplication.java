723e1387065cbddea7dbeb7fd2ab5fd02ef132eb
neutral00 <to.arvmail@gmail.com>
2025-12-12 19:52:57 +0530

    main class updated

--- src/main/java/com/lousing/poc/PocSpringbootApplication.java
+++ src/main/java/com/lousing/poc/PocSpringbootApplication.java
@@ -1,13 +1,12 @@
 package com.lousing.poc;
 
-import com.lousing.poc.config.NotificationConfig;
+import com.lousing.poc.config.ScopeConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(NotificationConfig.class)  // Load our Java config
+@Import(ScopeConfig.class)  // Load our Java config
 public class PocSpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(PocSpringbootApplication.class, args);
