84f3ecb2a64ab9fcef7ccc8a2759cdeffd96a212
neutral00 <to.arvmail@gmail.com>
2025-12-14 08:41:36 +0530

    main application class updated

--- src/main/java/com/lousing/poc/PocSpringbootApplication.java
+++ src/main/java/com/lousing/poc/PocSpringbootApplication.java
@@ -1,15 +1,18 @@
 package com.lousing.poc;
 
+import com.lousing.poc.config.ProfileConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(ProfileConfig.class)
 public class PocSpringbootApplication {
 
 	public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(PocSpringbootApplication.class, args);
-        System.out.println("✅ Properties & Profiles Ready!");
+        System.out.println("✅ Profiles Ready!");
         System.out.println("----------------------------------");
 	}
 }
